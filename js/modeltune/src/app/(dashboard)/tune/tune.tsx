09ebfe0ff789d35d96e00229f2b791685ff11008
Neal Chandra <chandra.neal@gmail.com>
2023-07-07 02:23:16 -0700

    wip

--- js/modeltune/src/app/(dashboard)/tune/tune.tsx
+++ js/modeltune/src/app/(dashboard)/tune/tune.tsx
@@ -50,6 +50,7 @@ import {
   TableHeader,
   TableRow,
 } from '@app/components/ui/table';
+import { Textarea } from '@app/components/ui/textarea';
 import { cn } from '@app/lib/utils';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { debounce } from 'lodash';
@@ -59,6 +60,8 @@ import { useForm } from 'react-hook-form';
 import { fromTheme } from 'tailwind-merge';
 import * as z from 'zod';
 import { string } from 'zod';
+
+import { DatasetPreview } from './dataset-preview';
 
 function zodEnumFromObjValues<K extends string>(
   obj: Record<any, K>
@@ -77,8 +80,10 @@ const formSchema = z.object({
   baseModel: baseModelChoices,
   dataset: z.object({
     id: z.string(),
+    id_case_sensitive: z.string(),
     private: z.boolean(),
   }),
+  promptTemplate: z.string().nonempty(),
   // epochs: z
   //   .number()
   //   .min(1, {
@@ -92,9 +97,9 @@ const formSchema = z.object({
 });
 
 export default function Tune() {
-  // const [data, setData] = React.useState<Array<Object>>();
+  const [open, setOpen] = React.useState(false);
   const [datasets, setDatasets] = React.useState<
-    Array<{ value: string; label: string; private: boolean }>
+    Array<{ id: string; value: string; label: string; private: boolean }>
   >([]);
   const [features, setFeatures] = React.useState<Array<string>>([]);
   const [submissionData, setSubmissionData] =
@@ -106,12 +111,15 @@ export default function Tune() {
       name: '',
       baseModel: BASE_MODELS.FALCON,
       // epochs: 1,
+      promptTemplate: '',
       feature: '',
       dataset: undefined,
       wandbKey: null,
     },
   });
   const datasetWatch = form.watch('dataset');
+  const featureWatch = form.watch('feature');
+  const promptTemplateWatch = form.watch('promptTemplate');
   const isPrivateDataset = form.watch('dataset.private');
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
@@ -141,11 +149,19 @@ export default function Tune() {
     if (!dataset) return;
 
     if (!dataset.private) {
-      getDatasetInfo(dataset.id).then((response) => {
-        setFeatures(Object.keys(response.dataset_info.features));
+      console.log('calling');
+      getDatasetInfo(dataset.id_case_sensitive).then((response) => {
+        console.log(response);
+        const features = Object.keys(response.dataset_info.features);
+        setFeatures(features);
+        form.setValue('feature', features?.[0]);
       });
     }
   }, [datasetWatch]);
+
+  React.useEffect(() => {
+    form.setValue('promptTemplate', `{{${featureWatch}}}`);
+  }, [featureWatch]);
 
   const updateDatasets = (e: Event) =>
     autocompleteDatasets((e.target as HTMLInputElement).value).then((resp) => {
@@ -185,174 +201,200 @@ export default function Tune() {
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Name</FormLabel>
-              <FormControl>
-                <Input placeholder="storywriter-lora" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is the name of your finetuned model
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="baseModel"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Base Model</FormLabel>
-              <Select onValueChange={field.onChange}>
-                <FormControl>
-                  <SelectTrigger>
-                    <SelectValue placeholder="Select base model" />
-                  </SelectTrigger>
-                </FormControl>
-                <SelectContent>
-                  {(
-                    Object.keys(BASE_MODELS) as Array<keyof typeof BASE_MODELS>
-                  ).map((key) => (
-                    <SelectItem key={key} value={BASE_MODELS[key]}>
-                      {BASE_MODEL_NAMES[BASE_MODELS[key]]}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-              <FormDescription>
-                The base model to begin finetuning from.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="dataset"
-          render={({ field }) => (
-            <FormItem className="flex flex-col">
-              <FormLabel>Dataset</FormLabel>
-              <Popover>
-                <PopoverTrigger asChild>
-                  <FormControl>
-                    <Button
-                      variant="outline"
-                      role="combobox"
-                      className={cn(
-                        'w-[400px] justify-between',
-                        !field.value && 'text-muted-foreground'
-                      )}
-                    >
-                      {field.value
-                        ? datasets.find(
-                            (dataset) => dataset.value === field.value?.id
-                          )?.label
-                        : 'Select dataset'}
-                      <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                    </Button>
-                  </FormControl>
-                </PopoverTrigger>
-                <PopoverContent className="w-[400px] p-0">
-                  <Command>
-                    <CommandInput
-                      placeholder="Enter dataset name..."
-                      onInputCapture={
-                        updateDatasetsDebounced as unknown as React.FormEventHandler<HTMLInputElement>
-                      }
-                    />
-                    <CommandEmpty>No dataset found.</CommandEmpty>
-                    <CommandGroup>
-                      {datasets.map((dataset) => (
-                        <CommandItem
-                          value={dataset.value}
-                          key={dataset.value}
-                          onSelect={(value) => {
-                            form.setValue('feature', '');
-                            form.setValue('dataset', {
-                              id: value,
-                              private: dataset.private,
-                            });
-                          }}
-                        >
-                          <Check
-                            className={cn(
-                              'mr-2 h-4 w-4',
-                              dataset.value === field.value?.id
-                                ? 'opacity-100'
-                                : 'opacity-0'
-                            )}
-                          />
-                          {dataset.label}
-                        </CommandItem>
-                      ))}
-                    </CommandGroup>
-                  </Command>
-                </PopoverContent>
-              </Popover>
-              <FormDescription>
-                This is the huggingface dataset to use for training
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        {!isPrivateDataset && features.length ? (
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
           <FormField
             control={form.control}
-            name="feature"
+            name="name"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Dataset Key</FormLabel>
-                <Select
-                  disabled={!features.length}
-                  onValueChange={field.onChange}
-                >
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="storywriter-lora" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is the name of your finetuned model
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="baseModel"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Base Model</FormLabel>
+                <Select onValueChange={field.onChange}>
                   <FormControl>
                     <SelectTrigger>
-                      <SelectValue placeholder="Select dataset feature" />
+                      <SelectValue placeholder="Select base model" />
                     </SelectTrigger>
                   </FormControl>
                   <SelectContent>
-                    {features.map((feature) => (
-                      <SelectItem key={feature} value={feature}>
-                        {feature}
+                    {(
+                      Object.keys(BASE_MODELS) as Array<
+                        keyof typeof BASE_MODELS
+                      >
+                    ).map((key) => (
+                      <SelectItem key={key} value={BASE_MODELS[key]}>
+                        {BASE_MODEL_NAMES[BASE_MODELS[key]]}
                       </SelectItem>
                     ))}
                   </SelectContent>
                 </Select>
                 <FormDescription>
-                  The key of the feature from the dataset to use for training
+                  The base model to begin finetuning from.
                 </FormDescription>
                 <FormMessage />
               </FormItem>
             )}
           />
-        ) : null}
-        {isPrivateDataset ? (
           <FormField
             control={form.control}
-            name="feature"
+            name="dataset"
             render={({ field }) => (
-              <FormItem>
-                <FormLabel>Dataset Key</FormLabel>
-                <FormControl>
-                  <Input type="text" placeholder="text" {...field} />
-                </FormControl>
+              <FormItem className="flex flex-col">
+                <FormLabel>Dataset</FormLabel>
+                <Popover open={open} onOpenChange={setOpen}>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button
+                        variant="outline"
+                        role="combobox"
+                        className={cn(
+                          'w-[400px] justify-between',
+                          !field.value && 'text-muted-foreground'
+                        )}
+                      >
+                        {field.value
+                          ? datasets.find(
+                              (dataset) => dataset.value === field.value?.id
+                            )?.label
+                          : 'Select dataset'}
+                        <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-[400px] p-0">
+                    <Command>
+                      <CommandInput
+                        placeholder="Enter dataset name..."
+                        onInputCapture={
+                          updateDatasetsDebounced as unknown as React.FormEventHandler<HTMLInputElement>
+                        }
+                      />
+                      <CommandEmpty>No dataset found.</CommandEmpty>
+                      <CommandGroup>
+                        {datasets.map((dataset) => (
+                          <CommandItem
+                            value={dataset.value}
+                            key={dataset.value}
+                            onSelect={(value) => {
+                              setFeatures([]);
+                              form.setValue('feature', '');
+                              form.setValue('dataset', {
+                                id: dataset.value,
+                                id_case_sensitive: dataset.id,
+                                private: dataset.private,
+                              });
+                              setOpen(false);
+                            }}
+                          >
+                            <Check
+                              className={cn(
+                                'mr-2 h-4 w-4',
+                                dataset.value === field.value?.id
+                                  ? 'opacity-100'
+                                  : 'opacity-0'
+                              )}
+                            />
+                            {dataset.label}
+                          </CommandItem>
+                        ))}
+                      </CommandGroup>
+                    </Command>
+                  </PopoverContent>
+                </Popover>
                 <FormDescription>
-                  The key of the feature from the dataset to use for training
+                  This is the huggingface dataset to use for training
                 </FormDescription>
                 <FormMessage />
               </FormItem>
             )}
           />
-        ) : null}
-        {/* <FormField
+          {!isPrivateDataset && features.length ? (
+            <FormField
+              control={form.control}
+              name="feature"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Dataset Key</FormLabel>
+                  <Select
+                    disabled={!features.length}
+                    onValueChange={field.onChange}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select dataset feature" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {features.map((feature) => (
+                        <SelectItem key={feature} value={feature}>
+                          {feature}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <FormDescription>
+                    The key of the feature from the dataset to use for the base
+                    prompt template.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          ) : null}
+          {isPrivateDataset ? (
+            <FormField
+              control={form.control}
+              name="feature"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Dataset Key</FormLabel>
+                  <FormControl>
+                    <Input type="text" placeholder="text" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    The key of the feature from the dataset to use for training
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          ) : null}
+          {featureWatch ? (
+            <FormField
+              control={form.control}
+              name="promptTemplate"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Prompt Template</FormLabel>
+                  <FormControl>
+                    <Textarea {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    The template to use to generate training data. Preview is
+                    shown on the right.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          ) : null}
+          {/* <FormField
           control={form.control}
           name="epochs"
           render={({ field }) => (
@@ -366,46 +408,32 @@ export default function Tune() {
             </FormItem>
           )}
         /> */}
-        <FormField
-          control={form.control}
-          name="wandbKey"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>wandb API Key</FormLabel>
-              <FormControl>
-                <Input
-                  type="text"
-                  placeholder="abc-123"
-                  {...field}
-                  value={field.value ?? ''}
-                />
-              </FormControl>
-              <FormDescription>Weights and Biases API Key</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-      {/* <Table>
-        <TableCaption>A list of your dataset.</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px]">Input</TableHead>
-            <TableHead>Instruction</TableHead>
-            <TableHead>Output</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {(data?.slice(0, 10) ?? []).map((row) => (
-            <TableRow key={row.row_idx}>
-              <TableCell className="font-medium">{row.row.input}</TableCell>
-              <TableCell>{row.row.instruction}</TableCell>
-              <TableCell>{row.row.output}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table> */}
-    </Form>
+          <FormField
+            control={form.control}
+            name="wandbKey"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>wandb API Key</FormLabel>
+                <FormControl>
+                  <Input
+                    type="text"
+                    placeholder="abc-123"
+                    {...field}
+                    value={field.value ?? ''}
+                  />
+                </FormControl>
+                <FormDescription>Weights and Biases API Key</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+      <DatasetPreview
+        dataset_id={!featureWatch ? '' : datasetWatch?.id_case_sensitive}
+        prompt_template={promptTemplateWatch}
+      />
+    </>
   );
 }
