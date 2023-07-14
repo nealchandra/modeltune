36370c7c6bb76d7300338eb3ea2b928a62f402fe
Neal Chandra <chandra.neal@gmail.com>
2023-07-13 23:46:01 -0700

    Cleanup and add resend email auth

--- js/modeltune/src/app/(auth)/login/user-auth-form.tsx
+++ js/modeltune/src/app/(auth)/login/user-auth-form.tsx
@@ -3,7 +3,9 @@
 import * as React from 'react';
 
 import { Icons } from '@app/components/icons';
-import { buttonVariants } from '@app/components/ui/button';
+import { Button, buttonVariants } from '@app/components/ui/button';
+import { Input } from '@app/components/ui/input';
+import { Label } from '@app/components/ui/label';
 import { cn } from '@app/lib/utils';
 import { signIn } from 'next-auth/react';
 import { useSearchParams } from 'next/navigation';
@@ -11,6 +13,7 @@ import { useSearchParams } from 'next/navigation';
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
+  const [email, setEmail] = React.useState<string>('');
   const [isGitHubLoading, setIsGitHubLoading] = React.useState<boolean>(false);
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
 
@@ -19,13 +22,41 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
 
   return (
     <div className={cn('grid gap-6', className)} {...props}>
+      <div className="grid gap-2">
+        <div className="grid gap-1">
+          <Label className="sr-only" htmlFor="email">
+            Email
+          </Label>
+          <Input
+            id="email"
+            placeholder="name@example.com"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            autoCapitalize="none"
+            autoComplete="email"
+            autoCorrect="off"
+            disabled={isLoading}
+          />
+        </div>
+        <Button
+          onClick={() => {
+            setIsLoading(true);
+            signIn('email', { email, callbackUrl });
+          }}
+          disabled={isLoading}
+        >
+          {isLoading && <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />}
+          Sign In with Email
+        </Button>
+      </div>
       <div className="relative">
         <div className="absolute inset-0 flex items-center">
           <span className="w-full border-t" />
         </div>
         <div className="relative flex justify-center text-xs uppercase">
           <span className="bg-background px-2 text-muted-foreground">
-            Choose a login method
+            Or continue with
           </span>
         </div>
       </div>
@@ -34,7 +65,7 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
         className={cn(buttonVariants({ variant: 'outline' }))}
         onClick={() => {
           setIsGitHubLoading(true);
-          signIn('github', { callbackUrl: callbackUrl });
+          signIn('github', { callbackUrl });
         }}
         disabled={isLoading || isGitHubLoading}
       >
--- js/modeltune/src/lib/session.ts
+++ js/modeltune/src/lib/session.ts
@@ -3,6 +3,5 @@ import { getServerSession } from 'next-auth/next';
 
 export async function getCurrentUser() {
   const session = await getServerSession(authOptions);
-
-    return session?.user;
+  return session?.user;
 }
